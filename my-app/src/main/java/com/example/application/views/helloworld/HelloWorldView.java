a46dc6da43f3919b3767504df1b93145ecfe50c9
EKayatz <eliaskayatz04@gmail.com>
2024-02-15 07:09:30 +0100

    ...

---
+++ my-app/src/main/java/com/example/application/views/helloworld/HelloWorldView.java
@@ -0,0 +1,26 @@
+package com.example.application.views.helloworld;
+
+import com.example.application.views.MainLayout;
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+
+@PageTitle("Hello World")
+@Route(value = "hello", layout = MainLayout.class)
+@RouteAlias(value = "", layout = MainLayout.class)
+public class HelloWorldView extends HorizontalLayout {
+
+    private TextField name;
+    private Button sayHello;
+
+    public HelloWorldView() {
+      add (new H1("Hello WOrld!"));
+    }
+
+}
